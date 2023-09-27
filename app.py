a035eae05c742ad69b6c84d72ff4ea1f3a041223
Bharat-Yadav-11 <142567000+Bharat-Yadav-11@users.noreply.github.com>
2023-09-27 22:46:57 +0530

    update

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mydb.db'
 db = SQLAlchemy(app)
 
 
